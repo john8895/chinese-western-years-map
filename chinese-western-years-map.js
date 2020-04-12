5f5938a7a319857b24884219f05c54538e3dd09e
JohnChen <paiching01@gmail.com>
2020-04-12 18:48:34 +0800

    update

--- chinese-western-years-map.js
+++ chinese-western-years-map.js
@@ -1,3 +1,10 @@
+window.addEventListener('load', focusFun);
+
+function focusFun() {
+    const input = document.getElementById('filterText');
+    input.focus();
+}
+
 /*
 * -------------------
 * 篩選表格資料
@@ -8,20 +15,40 @@ function filterList() {
     const table = document.getElementById('year-table');
     const trObj = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
 
+    //依 tr => td 依序搜尋比對，只要每一列比對到有相同的資料，就顯示該列
     for (let i = 0; i < trObj.length; i++) {
         const tdObj = trObj[i].getElementsByTagName('td');
         let limit = 0;
-
         for (let j = 0; j < tdObj.length; j++) {
             const textValue = tdObj[j].innerText || tdObj[j].textContent;
             if (textValue.indexOf(input) > -1) {
-                // console.log(textValue);
                 limit++;
             }
         }
+        //如果比對到資料，就給table-row，否則隱藏
         trObj[i].style.display = limit > 0 ? "table-row" : "none";
+
+        //如果全部比對不到，顯示「查不到資料」欄位
+        // const prompt = document.getElementById('prompt');
+        // prompt.style.display = limit > 0 ? "none" : "table-row";
     }
+
+
 }
+
+// function filterNotData(){
+//     const table = document.getElementById('year-table');
+//     const trObj = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+//
+//     for (let i = 0; i < trObj.length; i++) {
+//         console.log(trObj[i].style.display);
+//         console.log(trObj.length);
+//         // p_limit = trObj[i].style.display === "none" ? p_limit++ : p_limit;
+//         // console.log(p_limit);
+//
+//
+//     }
+// }
 
 /*
 * -------------------
